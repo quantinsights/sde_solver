e05e0d6331158a5195e4710060fefbcdcb62d1a3
quantophile <quasar.chunawala@gmail.com>
2024-07-11 11:31:42 +0200

    Implementation of Euler and Milstein methods

---
+++ sde_solver/milstein.py
@@ -0,0 +1,47 @@
+"""
+A module for Milstein discretization algorithm.
+"""
+
+from typing import Callable
+from dataclasses import dataclass, field
+import numpy as np
+from sivp import SIVP
+from solver import Solver
+
+
+@dataclass
+class Milstein(Solver):
+    """
+    Numerical solver for a stochastic differential equation(SDE) using
+    the Euler-Maruyama method.
+
+    Consider an SDE of the form :
+
+    dX_t = mu(t,X_t) dt + sigma(t,X_t) dB_t
+
+    with initial condition X_0 = x_0
+
+    The solution to the SDE can be computed using the increments
+
+    X_{n+1} - X_n = mu(n,X_n)(t_{n+1}-t_n) + sigma(n,X_n)(B(n+1)-B(n))
+    + 0.5 * sigma(n,X_n) * sigma'(n,X_n) * ((B(n+1) - B(n))**2 - (t_{n+1} - t_n))
+
+    """
+
+    def iterate(self, sivp: SIVP):
+        """
+        Generate the next iterate X(n+1)
+        """
+        current_time = self.iter * self.step_size
+        mu_n = sivp.drift(current_time, self.x_values[self.iter])
+        sigma_n = sivp.vol(current_time, self.x_values[self.iter])
+        dvol_dx_n = sivp.dvol_dx(current_time, self.x_values[self.iter])
+
+        delta_x = (
+            mu_n * self.step_size
+            + sigma_n * self.brownian_increments[self.iter]
+            + 0.5 * sigma_n * dvol_dx_n *
+            (self.brownian_increments[self.iter]**2 - self.step_size)
+        )
+
+        return self.x_values[self.iter] + delta_x
---
+++ sde_solver/solver.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from sivp import SIVP
+import numpy as np
+
+# More descriptive type hints
+T = np.ndarray
+X = np.ndarray
+
+@dataclass
+class Solver(ABC):
+    """
+    An abstract base class for all numerical schemes
+    """
+    num_steps: int = 100
+    num_paths: int = 100
+
+    def __post_init__(self):
+        self.iter = 0
+        self.x_values = np.zeros((self.num_paths,self.num_steps))
+        self.step_size = 1./self.num_steps
+        self.brownian_increments = np.sqrt(self.step_size) * np.random.standard_normal(
+            size=(self.num_paths, self.num_steps)
+        )
+        self.brownian = np.cumsum(self.brownian_increments, axis=1)
+        self.brownian = np.concatenate(
+            [np.zeros(shape=(self.num_paths, 1)), self.brownian], axis=1
+        )
+
+    @abstractmethod
+    def iterate(self, sivp: SIVP) -> X:
+        """
+        Compute the next iterate X(n+1)
+        """
+
+    def solve(self, sivp: SIVP) -> (T,X):
+        """
+        Solve the SIVP
+        """
+        self.x_values[0] = np.full(shape=(self.num_paths,),fill_value=sivp.initial_condition)
+        while self.iter < self.num_steps:
+            self.x_values[self.iter] = self.iterate(sivp)
+            self.iter += 1
+
+        times = np.linspace(sivp.t_start, sivp.t_end, self.num_steps+1)
+        return times, self.x_values
+
+    def reset(self):
+        self.__post_init__()
